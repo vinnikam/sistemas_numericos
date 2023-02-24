1751085ec08631f2a4c69b7aef4cfc59ed6b5bee
Vnni 2023 <>
2023-02-23 20:47:34 -0500

    Proyecto base para sistemas numericos.

---
+++ src/logica/Operaciones.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logica;
+
+/**
+ *
+ * @author Vinni
+ */
+public class Operaciones {
+    
+    public String convertirDecimalBinario(String valor){
+        return "";
+    }
+    public String convertirDecimalHexadecimal(String valor){
+        return "";
+    }
+    public String convertirDecimalOctal(String valor){
+        return "";
+    }
+    public String convertirBinarioDecimal(String valor){
+        return "";
+    }
+    public String convertirBinarioHexadecimal(String valor){
+        return "";
+    }
+    public String convertirBinarioOctal(String valor){
+        return "";
+    }
+    public String convertirHexadecimalBinario(String valor){
+        return "";
+    }
+    public String convertirHexadecimalDecimal(String valor){
+        return "";
+    }
+    public String convertirHexadecimalOctal(String valor){
+        return "";
+    }
+    public String convertirOctalBinario(String valor){
+        return "";
+    }
+    public String convertirOctalDecimal(String valor){
+        return "";
+    }
+    public String convertirOctalHexadecimal(String valor){
+        return "";
+    }
+    
+}
